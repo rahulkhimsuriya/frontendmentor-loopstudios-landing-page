31f43f456c061b45b0277254111592f3ffbb817f
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2021-01-09 19:11:23 +0530

    add interactive section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div></div>
+  <div class="w-full mt-20 space-y-20">
+    <div class="grid grid-cols-1 lg:grid-cols-2">
+      <div>
+        <img src="~assets/images/desktop/image-interactive.jpg" />
+      </div>
+
+      <div
+        class="text-center lg:text-left lg:px-16 pt-16 lg:mt-auto lg:-ml-20 bg-white"
+      >
+        <h2 class="uppercase text-gray-900 text-5xl font-light leading-none">
+          The leader in interactive VR
+        </h2>
+        <p class="mt-8 text-sm text-gray-600">
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
